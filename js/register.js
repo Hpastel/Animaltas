43aca4035b5ba459335c77817e02a5d666746e11
pabga <pedrobispocat@gmail.com>
2024-05-27 15:56:39 +0100

    resgistro validation

--- js/register.js
+++ js/register.js
@@ -8,7 +8,31 @@ document.getElementById('registerForm').addEventListener('submit', (e) => {
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
+    const error_message = document.getElementById('error')
 
+    if (name.trim() === '') {
+        error_message.textContent = ('Nome: Introduz um nome valido');
+        return;
+    }else if(name.trim().lenght < 3){
+        error_message.textContent = ('Nome: Minimo 3 caracteres no nome');
+    }else{
+        error_message.textContent = '';
+    }
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!emailRegex.test(email)) {
+        error_message.textContent = ('Email: Introduz um email');
+        return;
+    }else{
+        error_message.textContent = '';
+    }
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+    if (!passwordRegex.test(password)) {
+        error_message.textContent = ('Password: Minimo 8 caracteres, um numero, uma letra maiusculo, uma letra minuscula');
+        return;
+    }else{
+        error_message.textContent = '';
+    }
+    
     auth.createUserWithEmailAndPassword(email, password)
         .then((userCredential) => {
             const user = userCredential.user;
